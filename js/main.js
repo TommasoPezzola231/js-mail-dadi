acabe4ad515e1b036e2217389d92614085f23f7a
TommasoPezzola <pezzola123@gmail.com>
2023-04-18 16:17:38 +0200

    Seconda parte

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 const email = ["utente1@gmail.com", "utente2@gmail.com", "utente3@gmail.com", "utente4@gmail.com", "utente5@gmail.com", "utente6@gmail.com"]
 
-let emailUtente = prompt("Inserisci la tua Email");
+let emailUtente = document.getElementById("emailUtente");
+
+let button = document.getElementById("verifica");
+
+let messaggio = document.getElementById("risposta");
+
 
 let key = "NON è";
 
+button.addEventListener("click",
+    function () {
 
-for (let i = 0; i < email.length; i++) {
+        for (let i = 0; i < email.length; i++) {
 
-    if (emailUtente == email [i]) {
-        key = "è"
+            let emailUtente = document.getElementById("emailUtente").value;
+        
+            if (emailUtente == email [i]) {
+                key = "è"
+            } 
+        
+        }
+
+        messaggio = `L'utente ${key} Registrato!`;
+        console.log(`${messaggio}`);
+        messaggio.innerText = messaggio;
+        
     }
-
-}
-
-console.log(`L'utente ${key} Registrato!`);
-alert(`L'utente ${key} Registrato!`);
+)
